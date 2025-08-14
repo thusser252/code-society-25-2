5eaea0a536e4217acf46115049b2009d50eb878b
txjackson252 <txjackson252@gmail.com>
2025-08-14 09:51:22 -0400

    feat: adds lesson_03 trinitiejackson quiz (#108)
    * trinitie_jackson_quiz
    
    * change closed to void
    
    * change tag to element
    
    * change question and answers
    
    * add anthony_mays_quiz back
    
    * edit spacing
    
    ---------
    
    Co-authored-by: Trinitie Jackson <trinitiejackson@Trinities-MacBook-Air.local>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { TrinitieJacksonQuiz } from './trinitie_jackson_quiz.js';
 import { TyranRicesQuiz } from './tyran_rices_quiz.js';
 import { BrooklynHardenQuiz } from './brooklyn_harden_quiz.js';
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
 
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, TyranRicesQuiz, BrooklynHardenQuiz];
+const QUIZ_PROVIDERS = [
+  AnthonyMaysQuiz, 
+  TrinitieJacksonQuiz, 
+  BrooklynHardenQuiz,
+  TyranRicesQuiz,
+  AnotherQuiz,
+
+];
 
 @Module({
   providers: [
