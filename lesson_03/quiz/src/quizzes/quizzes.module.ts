1fa2290361c1ae1c6e3ae6dcc47541163945ea13
Kerry Ferguson <112345520+KerryFerguson04@users.noreply.github.com>
2025-08-16 11:52:27 -0400

    feat: Adds Kerry Ferguson's Lesson_03 Quiz (#106)
    * feat: Adds Kerry Ferguson's Lesson_03 Quiz
    
    * chore: removes  extra line breaks
    
    * Update quizzes.module.ts
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DanielsonAdjocyQuiz } from './danielson_adjocys_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { KerryFergusonQuiz } from './kerry_ferguson_quiz.js';
 import { BenjaminScottQuiz } from './benjamin_scott_quiz.js';
 import { TrinitieJacksonQuiz } from './trinitie_jackson_quiz.js';
 import { TyranRicesQuiz } from './tyran_rices_quiz.js';
 import { BrooklynHardenQuiz } from './brooklyn_harden_quiz.js';
+
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
@@ -20,6 +22,7 @@ const QUIZ_PROVIDERS = [
   TyranRicesQuiz,
   AnotherQuiz,
   BenjaminScottQuiz,
+  KerryFergusonQuiz,
   DanielsonAdjocyQuiz
 ];
 
