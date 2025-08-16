233ff976f0c5b272de7f5343ab30899ef38571b8
dahw <51380247+dahw@users.noreply.github.com>
2025-08-16 11:58:24 -0400

    feat: adds Dean's custom quiz (#112)
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -7,25 +7,22 @@ import { BenjaminScottQuiz } from './benjamin_scott_quiz.js';
 import { TrinitieJacksonQuiz } from './trinitie_jackson_quiz.js';
 import { TyranRicesQuiz } from './tyran_rices_quiz.js';
 import { BrooklynHardenQuiz } from './brooklyn_harden_quiz.js';
+import { DeanWalstonQuiz } from './dean_walston_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-
-
-
-
 const QUIZ_PROVIDERS = [
-  AnthonyMaysQuiz, 
-  TrinitieJacksonQuiz, 
+  AnthonyMaysQuiz,
+  TrinitieJacksonQuiz,
   BrooklynHardenQuiz,
   TyranRicesQuiz,
   AnotherQuiz,
   BenjaminScottQuiz,
+  DanielsonAdjocyQuiz,
+  DeanWalstonQuiz,
   KerryFergusonQuiz,
-  DanielsonAdjocyQuiz
 ];
-
 
 @Module({
   providers: [
