c21621c3ebaead224d832cbad44507c3e82ce5c8
thusser252 <thusser252@users.noreply.github.com>
2025-08-20 15:10:40 +0000

    Implement expression calculator with correct method calls

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,29 @@
-export class ExpressionCalculator {
-  /** Returns a calculation involving a, b, c, d, and e */
-  calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
-  }
+import type { Calculator } from "./calculator.js";
 
-  pow(base: number, exponent: number): number {
-    return Math.pow(base, exponent);
-  }
+export class ExpressionCalculator implements Calculator {
+  /** Returns a calculation involving a, b, c, d, and e */
+    calculate = (a: number, b: number, c: number, d: number, e: number): number => {
+      const powerResult = this.pow(d, e);
+      const multiplyResult = this.multiply(c, powerResult);
+      const divideResult = this.divide(a, b);
+      return this.add(divideResult, multiplyResult);
+    };
+
+    pow = (base: number, exponent: number): number => {
+      return Math.pow(base, exponent);
+    };
+
+    add = (a: number, b: number): number => {
+      return a + b;
+    };
+
+    divide = (a: number, b: number): number => {
+      return a / b;
+    };
+
+    multiply = (a: number, b: number): number => {
+      return a * b;
+    };
+
+
 }
