8a0f9a3c3b73205b2e643849182979261f60b6f1
Joy Brown <jxbrown252@gmail.com>
2025-08-22 11:06:01 -0400

    feat: adds Joy's lesson03 quiz (#309)
    * add my quiz
    
    * feat: Added the required dependencies for the tests to pass
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { DanielsonAdjocyQuiz } from './danielson_adjocys_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { JoyBrownQuiz } from './joy_brown_quiz.js';
 import { CalvinRobinsonQuiz } from './calvin_robinson_quiz.js';
 import { DanielBoyceQuiz } from './daniel_boyce_quiz.js';
 import { NicoleJacksonQuiz } from './nicole_jackson_quiz.js';
@@ -31,6 +32,7 @@ const QUIZ_PROVIDERS = [
   DevynBensonQuiz,
   TyranRicesQuiz,
   AnotherQuiz,
+  JoyBrownQuiz,
   CalvinRobinsonQuiz,
   MattieWeathersbyQuiz,
   BenjaminScottQuiz,
