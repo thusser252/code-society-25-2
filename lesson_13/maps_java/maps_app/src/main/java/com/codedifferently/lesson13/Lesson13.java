2ca1a22ce3f96ead71290e40b30ad27bea65e01f
Student <student@codesociety.com>
2025-09-05 16:25:09 +0000

    Implement findPermutationDifference method in Lesson13

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,26 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    // Create a map to store character positions in string s
+    Map<Character, Integer> charPositions = new HashMap<>();
+    
+    // Store the position of each character in string s
+    for (int i = 0; i < s.length(); i++) {
+      charPositions.put(s.charAt(i), i);
+    }
+    
+    int permutationDifference = 0;
+    
+    // For each character in string t, find its position in s and calculate difference
+    for (int i = 0; i < t.length(); i++) {
+      char currentChar = t.charAt(i);
+      int positionInS = charPositions.get(currentChar);
+      int positionInT = i;
+      
+      // Add the absolute difference to the total
+      permutationDifference += Math.abs(positionInS - positionInT);
+    }
+    
+    return permutationDifference;
   }
 }
