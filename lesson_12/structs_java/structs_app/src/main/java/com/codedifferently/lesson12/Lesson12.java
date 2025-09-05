601232aaab64ad5dee871aa7cfcda15a6f7d78a7
thusser252 <thusser252@users.noreply.github.com>
2025-09-05 14:47:58 +0000

    revert: restore lesson 11

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,29 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int evenScore = 0;
+    int oddScore = 0;
+
+    ListNode current = head;
+    while (current != null && current.next != null) {
+      int evenValue = current.val; // Even-indexed node (0, 2, 4, ...)
+      int oddValue = current.next.val; // Odd-indexed node (1, 3, 5, ...)
+
+      if (evenValue > oddValue) {
+        evenScore++;
+      } else if (oddValue > evenValue) {
+        oddScore++;
+      }
+
+      current = current.next.next; // Move to next pair
+    }
+
+    if (evenScore > oddScore) {
+      return "Even";
+    } else if (oddScore > evenScore) {
+      return "Odd";
+    } else {
+      return "Tie";
+    }
   }
 }
