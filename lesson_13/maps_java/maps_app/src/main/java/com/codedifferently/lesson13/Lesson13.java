68c315cd1d857a0d6daba02e5235ea2a90635365
Student <student@codesociety.com>
2025-09-05 16:43:57 +0000

    fix: apply Spotless formatting to resolve build failures

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -12,24 +12,24 @@ public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
     // Create a map to store character positions in string s
     Map<Character, Integer> charPositions = new HashMap<>();
-    
+
     // Store the position of each character in string s
     for (int i = 0; i < s.length(); i++) {
       charPositions.put(s.charAt(i), i);
     }
-    
+
     int permutationDifference = 0;
-    
+
     // For each character in string t, find its position in s and calculate difference
     for (int i = 0; i < t.length(); i++) {
       char currentChar = t.charAt(i);
       int positionInS = charPositions.get(currentChar);
       int positionInT = i;
-      
+
       // Add the absolute difference to the total
       permutationDifference += Math.abs(positionInS - positionInT);
     }
-    
+
     return permutationDifference;
   }
 }
