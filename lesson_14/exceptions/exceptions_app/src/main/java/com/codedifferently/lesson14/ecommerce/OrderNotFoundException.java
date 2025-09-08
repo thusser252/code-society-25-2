0cca799a5c456ffb0897c196d09e2e377a5fd5ee
thusser252 <thusser252@users.noreply.github.com>
2025-09-08 14:45:17 +0000

    completed lesson 14 exceptions

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.codedifferently.lesson14.ecommerce;
 
-class OrderNotFoundException {}
+public class OrderNotFoundException extends Exception {
+  public OrderNotFoundException(String message) {
+    super(message);
+  }
+}
--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/ProductNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/ProductNotFoundException.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.codedifferently.lesson14.ecommerce;
 
-class ProductNotFoundException {}
+public class ProductNotFoundException extends Exception {
+  public ProductNotFoundException(String message) {
+    super(message);
+  }
+}
